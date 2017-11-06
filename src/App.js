bb6d8f04203a536016150955252b235e454cba14
LemonaInc <jaxonstevens@lemonainc.com>
2017-11-06 01:00:14 -0800

    Map now gets JSON data from the Translink API and parses the current latitude and longitde for the buses and shows the current location of the buses on the map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import MapGL from 'react-map-gl';
 import Options from './options';
 import Footer from './footer';
 import Request from 'request';
+import MapGL, {Marker} from 'react-map-gl';
+/* Import React Superagent */
+import Superagent from 'superagent';
 import AlertContainer from 'react-alert'
+/* Import React Material Buttons */
 import Button from 'material-ui/Button';
-import {defaultMapStyle, dataLayer} from './map-style.js';
 import {fromJS} from 'immutable';
+/* Import React MapBox Functions */
+import {defaultMapStyle, dataLayer} from './map-style.js';
+//import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactLogo from './logo.svg';
+import location from './currentcity.png';
+/* import Material Icons for the bus icon */
+import MaterialIcon from 'react-google-material-icons'
 
-const DEFAULT_QUERY = 'redux';
-const request = require('request');
+
 
 /* Define Constants */
+const DEFAULT_QUERY = 'redux';
+const request = require('request');
 /* Get a API token from Mapbox (https://www.mapbox.com) to load a map and use custom map styles */
 const MAPBOX_API_TOKEN = 'pk.eyJ1IjoiamF4b21hbiIsImEiOiJjajlsdHd3NnIwa3c4MndudHB3dDhmemtsIn0.Xif3OiwAP_X9nudmidWTag'; // Set your mapbox token here
 /* Define a Translink API URL to fetch live bus locations */
@@ -28,7 +38,28 @@ var isAPIConnectionActive = null;
 /* React App Extends Component */
 export default class App extends Component {
 
-/* React Alert Pop Up Options */
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    mapStyle: defaultMapStyle,
+    hoveredFeature: null,
+    viewport: {
+      /* Set the latitude and longitude to Vancouver coordinates */
+      latitude: 49.2827,
+      longitude: -123.1207,
+      /* Set the initial map zoom to 12 to see the vancouver area */
+      zoom: 12,
+      bearing: 0,
+      pitch: 0,
+      width: 500,
+      height: 500,
+   },
+   currentBusLocations:[]
+  }
+};
+
+  /* React Alert Pop Up Options */
   alertOptions = {
     offset: 14,
     position: 'top center',
@@ -41,7 +72,15 @@ export default class App extends Component {
     this.msg.show('Welcome to My Translink API Connector built with React.', {
       time: 8000,
       type: 'success',
-      icon: <img src="path/to/some/img/32x32.png" />
+      icon: <MaterialIcon icon="tag_faces" size={30} />
+    })
+  }
+
+  showIntroAlert = () => {
+    this.msg.show('The bus icon reflects a buses current location.', {
+      time: 8000,
+      type: 'success',
+      icon: <MaterialIcon icon="near_me" size={30} />
     })
   }
 
@@ -49,103 +88,108 @@ export default class App extends Component {
     this.msg.show('We encountered an error while fetching the data.', {
       time: 5000,
       type: 'success',
-      icon: <img src="path/to/some/img/32x32.png" />
+      icon:  <MaterialIcon icon="error_outline" size={30} />
     })
   }
-
-
 
   showSuccessAlert = () => {
     this.msg.show('The Translink API data was recieved', {
       time: 5000,
       type: 'success',
-      icon: <img src="path/to/some/img/32x32.png" />
+      icon: <MaterialIcon icon="directions_bus" size={30} />
     })
   }
+
 
   /* Create a function to be called when the user checks the connection */
   checkTranslinkAPIConnection() {
 
-  if (isAPIConnectionActive == true) {
+    if (isAPIConnectionActive == true) {
 
-console.log('Connection is active');
+      console.log('Connection is active');
 
-} else {
 
-  console.log('Connection is not active');
-}
+    } else {
 
-}
-
-  state = {
-    mapStyle: defaultMapStyle,
-    data: null,
-    hoveredFeature: null,
-    viewport: {
-      /* Set the latitude and longitude to Vancouver coordinates */
-      latitude: 49.2827,
-      longitude: -123.1207,
-      /* Set the initial map zoom to 12 to see the vancouver area */
-      zoom: 12,
-      bearing: 0,
-      pitch: 0,
-      width: 500,
-      height: 500
+      console.log('Connection is not active');
     }
 
-  };
+  }
 
 
-/* This function fetches the bus location from the translink API and gets the data back in JSON */
-fetchBusLocation() {
+  /* This function fetches the bus location from the translink API and gets the data back in JSON */
+  fetchBusLocation() {
 
-  request.get('/api', (req, res) => {
-    request({
-      /* Pass in the Translink API URL */
-      url: TRANSLINK_LIVE_API_URL,
-      method: "GET",
-      /* Return JSON data from the Translink API */
-      json: true
-    }, (err, response, body) => {
-      if (err || response.statusCode !== 200) {
-        console.log('Problem fetching API Data, please check the API KEY.', err);
-        /* Show the error alert if the data is not recived from the Translink */
-       this.showErrorAlert();
-       /* Set the isAPIConnectionActive to false */
-       isAPIConnectionActive = false;
-      } else {
-      console.log('body:', body);
-      /* Show the sucess alert if the data is recived from the Translink */
-      /* Set the isAPIConnectionActive to true */
-      isAPIConnectionActive = true;
-  /* If statement for checking the API Connection on componentDidMount */
-    if (displayCheckAPIAlert == true) {
-      this.showSuccessAlert();
-       displayCheckAPIAlert = false;
-    }
-      }
+    request.get('/api', (req, res) => {
+      request({
+        /* Pass in the Translink API URL */
+        url: TRANSLINK_LIVE_API_URL,
+        method: "GET",
+        /* Return JSON data from the Translink API */
+        json: true
+      }, (err, response, body) => {
+        if (err || response.statusCode !== 200) {
+          console.log('Problem fetching API Data, please check the API KEY.', err);
+          /* Show the error alert if the data is not recived from the Translink */
+          this.showErrorAlert();
+          /* Set the isAPIConnectionActive to false */
+          isAPIConnectionActive = false;
+        } else {
+
+       /* parse the body and get the latitude and longitude */
+          this.parseBusJSONCoordinates(body);
+          console.log('All Bus Details:', body);
+
+          /* Show the sucess alert if the data is recived from the Translink */
+          /* Set the isAPIConnectionActive to true */
+          isAPIConnectionActive = true;
+          /* If statement for checking the API Connection on componentDidMount */
+          if (displayCheckAPIAlert == true) {
+            this.showSuccessAlert();
+            displayCheckAPIAlert = false;
+          }
+        }
+      });
     });
-  });
 
-}
+  }
 
-/* The componentDidMount method gets called once the component is mounted to the DOM */
-/* Use Component did Mount for fetching the API data and calling methods when the web app loads */
+
+  parseBusJSONCoordinates(vancouverBusLocation) {
+    let currentBusLocationArray = vancouverBusLocation.map((theBus) => ({lat: theBus.Latitude, lon: theBus.Longitude}));
+    this.setState({currentBusLocations: currentBusLocationArray});
+    console.log('Bus Positions',currentBusLocationArray)
+
+  }
+
+/* Get the currrent location of the user and display the location on a map */
+  getCurrentLocation() {
+
+
+  }
+
+  /* The componentDidMount method gets called once the component is mounted to the DOM */
+  /* Use Component did Mount for fetching the API data and calling methods when the web app loads */
   componentDidMount() {
 
-   /* Call the fetchBusLocation function every defined amount of seconds */
+        /* Show the welcome alert */
+        this.showWelcomeAlert();
+        //this.getBusPoints();
+
+    /* Call the fetchBusLocation function every defined amount of seconds */
+
     this.timer = setInterval(()=> this.fetchBusLocation(), 1000)
 
-   /* Show the welcome alert */
-   this.showWelcomeAlert();
+    /* Show the welcome alert */
+    this.showIntroAlert();
 
-  /* Set the window size */
+    /* Set the window size */
     window.addEventListener('resize', this.windowSize);
     this.windowSize();
   }
 
 
-/* The componentWillUnmount method gets called right before the component is unloaded from the DOM */
+  /* The componentWillUnmount method gets called right before the component is unloaded from the DOM */
   componentWillUnmount() {
 
     /* Set the window size */
@@ -156,55 +200,68 @@ fetchBusLocation() {
     this.setState({
       viewport: {
         ...this.state.viewport,
-        width: 1920 || window.innerWidth,
-        height: 800 || window.innerHeight
+        width:   window.innerWidth,
+        height:  window.innerHeight
       }
     });
   };
 
- /* Window View Port */
+  /* Window View Port */
   windowViewPort = viewport => this.setState({viewport});
 
-/* on mouse hover event */
+  /* on mouse hover event */
   onMouseHover = event => {
 
   };
 
-
-
-  /* Everything in the render function will be rendered to the view */
+  /* Everything in the React render function will be rendered to the view */
   render() {
 
     const {viewport, mapStyle} = this.state;
 
     return (
       <div>
-     <center>  <h2>Translink Live Bus Stops</h2>
-     <div>
-     {/* React Welcome Alert Popup */}
-    <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
-    <Button raised color="accent" onClick={this.checkTranslinkAPIConnection}>Check API Connection</Button>
-    </div>
-     </center>
+      <center>  <h2>TransLink Live</h2>
+      <div>
+      {/* React Welcome Alert Popup */}
+      <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
+      <Button raised color="accent" onClick={this.checkTranslinkAPIConnection}>Check API Connection</Button>
+      <Button raised color="primary" onClick={this.checkTranslinkAPIConnection}>Get Current Location</Button>
 
-     {/* MapBox Map Integration */}
+      </div>
+      </center>
 
-        <MapGL
-          {...viewport}
-          mapStyle={mapStyle}
 
-          onViewportChange={this.windowViewPort}
-          mapboxApiAccessToken={MAPBOX_API_TOKEN}
-          onHover={this.onMouseHover} >
+      {/* MapBox Map Integration */}
+      <MapGL
+      {...viewport}
+      mapStyle={mapStyle}
 
-          {/* Load in the react Options Component */}
+      onViewportChange={this.windowViewPort}
+      mapboxApiAccessToken={MAPBOX_API_TOKEN}
+      onHover={this.onMouseHover}>
 
-        <Options containerComponent={this.props.containerComponent}/>
-        </MapGL>
+      {/* Mapbox Markers for the bus locations */}
+      <Marker latitude={49.2827} longitude={-123.1207}>
+           <div> <img src={location}/> </div>
+         </Marker>
 
-        {/* Webpage Footer Component*/}
-        <Footer containerComponent={this.props.containerComponent}/>
+         {/* Parse the JSON and get the longitude and latitude and display on the map */}
+
+         {this.state.currentBusLocations.map((position, index) => (
+           <Marker latitude={position.lat} longitude={position.lon} key={index}>
+           <div> <MaterialIcon icon="directions_bus" size={30}/> </div>
+           </Marker>
+         ))}
+
+
+      {/* Load in the react Options Component */}
+      <Options containerComponent={this.props.containerComponent}/>
+      </MapGL>
+
+      {/* Load in the react Webpage Footer Component */}
+      <Footer containerComponent={this.props.containerComponent}/>
       </div> );
-  }
+    }
 
-}
+  }
